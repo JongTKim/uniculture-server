e47246cc0a7b9efb6d58d303282c59108b9bc600
JongTKim <nh02111@naver.com>
2024-04-07 21:52:35 +0900

    MemberRepository 수정

--- uniculture/src/main/java/com/capstone/uniculture/repository/MemberRepository.java
+++ uniculture/src/main/java/com/capstone/uniculture/repository/MemberRepository.java
@@ -2,13 +2,15 @@ package com.capstone.uniculture.repository;
 
 import com.capstone.uniculture.entity.Member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member,Long> {
+public interface MemberRepository extends JpaRepository<Member,Long> , JpaSpecificationExecutor<Member> {
+
 
     // Email 주소로 Member 찾는 로직
     Optional<Member> findByEmail(String email);
@@ -23,4 +25,7 @@ public interface MemberRepository extends JpaRepository<Member,Long> {
     // 전체 멤버중 내 친구가 아닌 멤버 찾기
     @Query("SELECT m FROM Member m WHERE m.id NOT IN (SELECT f.toMember.id FROM Friendship f WHERE f.fromMember.id = :myId)")
     List<Member> findNonFriendMembers(@Param("myId") Long myId);
+
+    @Query("SELECT m FROM Member m JOIN fetch m.myHobbyList")
+    List<Member> findAll2();
 }
