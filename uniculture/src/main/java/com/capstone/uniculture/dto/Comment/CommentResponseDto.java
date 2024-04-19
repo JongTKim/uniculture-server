b7af22cc1de9f7204426a61e2aedaea067644be2
JongTKim <nh02111@naver.com>
2024-04-19 21:26:11 +0900

    대댓글 조회기능 개발에 따른 DTO 와 Repository 수정

--- uniculture/src/main/java/com/capstone/uniculture/dto/Comment/CommentResponseDto.java
+++ uniculture/src/main/java/com/capstone/uniculture/dto/Comment/CommentResponseDto.java
@@ -7,34 +7,42 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-@Getter
+@Getter @Setter
 @NoArgsConstructor
 public class CommentResponseDto {
 
-    private Long commentId;
+    private Long id;
     private String content;
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
     private String commentWriterName;
+    private Boolean isMine = Boolean.FALSE;
+    private Boolean postMine = Boolean.FALSE;
+    private Boolean isDeleted = Boolean.FALSE;
+    private List<CommentResponseDto> children = new ArrayList<>();
 
     @Builder
-    public CommentResponseDto(Long commentId, String content, LocalDateTime createdDate, LocalDateTime modifiedDate, String commentWriterName) {
-        this.commentId = commentId;
+    public CommentResponseDto(Long id, String content, LocalDateTime createdDate, LocalDateTime modifiedDate, String commentWriterName, Boolean isDeleted) {
+        this.id = id;
         this.content = content;
         this.createdDate = createdDate;
         this.modifiedDate = modifiedDate;
         this.commentWriterName = commentWriterName;
+        this.isDeleted = isDeleted;
     }
 
 
     public static CommentResponseDto fromEntity(Comment comment){
         return CommentResponseDto.builder()
-                .commentId(comment.getId())
+                .id(comment.getId())
                 .content(comment.getContent())
                 .createdDate(comment.getCreatedDate())
                 .modifiedDate(comment.getModifiedDate())
                 .commentWriterName(comment.getMember().getNickname())
+                .isDeleted(comment.getIsDeleted())
                 .build();
     }
 }
