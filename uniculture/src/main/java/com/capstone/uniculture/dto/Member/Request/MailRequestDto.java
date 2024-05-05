5fa141d8dd20ad913d73ffdae74148059506e03c
JongTKim <nh02111@naver.com>
2024-05-05 23:10:16 +0900

    이메일 인증 기능 구현

---
+++ uniculture/src/main/java/com/capstone/uniculture/dto/Member/Request/MailRequestDto.java
@@ -0,0 +1,12 @@
+package com.capstone.uniculture.dto.Member.Request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MailRequestDto {
+    private String email;
+}
